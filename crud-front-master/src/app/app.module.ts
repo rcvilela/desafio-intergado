024db501b27a6f7d91c44227d57e5fe7b92f6b52
Raíssa Vilela <rcarolvilela@hotmail.com.br>
2021-04-11 23:32:07 -0300

    Fixed module imports

--- crud-front-master/src/app/app.module.ts
+++ crud-front-master/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CadastroAnimaisComponent } from './main/cadastro-animais/cadastro-animais.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule, ToastrService, TOAST_CONFIG } from 'ngx-toastr';
 
 @NgModule({
   declarations: [AppComponent, CadastroAnimaisComponent],
--- crud-front-master/src/app/main/cadastro-animais/cadastro-animais.module.ts
+++ crud-front-master/src/app/main/cadastro-animais/cadastro-animais.module.ts
@@ -3,17 +3,16 @@ import { AppModule } from 'src/app/app.module';
 import { CadastroAnimaisComponent } from './cadastro-animais.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material';
-import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 
 @NgModule({
   declarations: [CadastroAnimaisComponent],
-  imports: [AppModule,
+  imports: [
+    AppModule,
     MatTableModule,
-    ToastrModule,
-    MatIconModule],
-  exports: [CadastroAnimaisComponent],
-  providers: [ToastrService]
+    MatIconModule
+  ],
+  exports: [CadastroAnimaisComponent]
 })
 
 export class CadastroAnimaisModule { }
